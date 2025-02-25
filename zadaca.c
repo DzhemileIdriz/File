64f80267d3a442b1112fadb5e6999cd47bb4bb6d
DzhemileIdriz <djemi_2002@abv.bg>
2025-02-25 15:17:08 +0200

    Update zadaca.c

--- zadaca.c
+++ zadaca.c
@@ -3,50 +3,63 @@
 #include <string.h>
 #define MAXN 1000
 
-char* cezar_cipher (char* str, int key)
+
+char* cezar_cipher(char* str, int key)
 {
-int length = strlen (str);
-char* cipher = (char*) malloc (sizeof(char) * (length + 1));
+    int length = strlen(str);
+    char* cipher = (char*)malloc(sizeof(char) * (length + 1));  
 
-for (int i = 0; i < length; i++){
-
-   if (str [i] >= 'a' && str [i] <= 'z') 
-    {cipher[i]=(str[i] - 'a' + key) % 26 +'a';}
-
-    else if (str [i] >= 'A' && str [i] <= 'Z') 
-    {cipher[i]=(str[i] - 'A' + key) % 26 +'Z';}
-
-
-    else if (str [i] >= '0' && str [i] <= '9') 
-    {cipher[i]=(str[i] - '0' + key) % 10 +'0';}
-    
-
-    else {
-        cipher [i] = str[i];
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z') 
+        {
+            cipher[i] = (str[i] - 'a' + key) % 26 + 'a';
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')  
+        {
+            cipher[i] = (str[i] - 'A' + key) % 26 + 'A';
+        }
+        else
+        {
+            cipher[i] = str[i];  
+        }
     }
 
-} 
-
-
-cipher[length] = '\0';
-return cipher;
-
-
+    cipher[length] = '\0';  
+    return cipher;
 }
 
-int main ()
+int main()
 {
-    char str[MAXN];
-    printf ("Enter text:" );
-    scanf ("%s", str);
+    char str[MAXN];  
+    printf("Enter a string: ");
+    fgets(str, MAXN, stdin);  
+
+    
+    str[strcspn(str, "\n")] = 0;
 
     int key;
-    printf("Enter key:" );
-    scanf ("%d", &key);
+    printf("Enter an encryption key (Whole number): ");
+    scanf("%d", &key);  
 
-    char* cipher = cezar_cipher (str, key);
-    printf ("Result: %s", cipher);
+    
+    char* cipher = cezar_cipher(str, key);
+    printf("Ciphertext: %s\n", cipher);
+
+    
+    FILE *file = fopen("ciphertext.txt", "w");  
+    if (file != NULL)
+    {
+        fprintf(file, "%s\n", cipher);  
+        fclose(file);  
+        printf("The ciphertext is saved to a file ciphertext.txt\n");
+    }
+    else
+    {
+        printf("Error opening file!\n");
+    }
+
+    free(cipher);
 
     return EXIT_SUCCESS;
-
 }
